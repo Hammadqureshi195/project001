66927ff53a465204c7bdd01b887c0ef53d06467e
Hammadqureshi195 <78595464+Hammadqureshi195@users.noreply.github.com>
2021-02-18 20:55:12 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />
-          Update by <i>Hammad Qureshi</i>
         </p>
         <a
           className="App-link"
